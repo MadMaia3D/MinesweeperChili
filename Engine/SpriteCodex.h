d65aa92b809ac664869d4cdac4efa37181762efc
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-07-22 14:40:02 -0300

    change: sprites size from 16 to 24

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -7,7 +7,7 @@ class SpriteCodex
 {
 public:
 	// width and height of all tiles
-	static constexpr int tileSize = 16;
+	static constexpr int tileSize = 24;
 	// base color for all tiles
 	static constexpr Color baseColor = { 192,192,192 };
 	// 16x16 tile sprites assume (192,192,192) background and top left origin
@@ -21,7 +21,8 @@ public:
 	static void DrawTile7( const Vei2& pos,Graphics& gfx );
 	static void DrawTile8( const Vei2& pos,Graphics& gfx );
 	static void DrawTileButton( const Vei2& pos,Graphics& gfx );
-	static void DrawTileCross( const Vei2& pos,Graphics& gfx );
+	static void DrawTileCrossRed( const Vei2& pos,Graphics& gfx );
+	static void DrawTileGreenCross(const Vei2& pos, Graphics& gfx);
 	static void DrawTileFlag( const Vei2& pos,Graphics& gfx );
 	static void DrawTileBomb( const Vei2& pos,Graphics& gfx );
 	static void DrawTileBombRed( const Vei2& pos,Graphics& gfx );
