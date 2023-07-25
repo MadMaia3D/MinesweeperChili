a7ef95c2cf22c20b71ecbe62975fd07d963c9a2e
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-07-25 00:18:35 -0300

    add: face drawing to the frame

--- Engine/MinesweeperGame.cpp
+++ Engine/MinesweeperGame.cpp
@@ -21,15 +21,26 @@ void MinesweeperGame::InputRightClick(const Vei2& clickPosition){
 
 void MinesweeperGame::Draw(Graphics& gfx) const {
 	static constexpr int bevelDepth = 4;
+	// draw frame outer
 	SpriteCodex::DrawBeveledSquareUp(gameRect, bevelDepth, gfx);
+
+	// draw minefield emboss
 	const RectI fieldRect = minefield.GetFieldRect().GetExpanded(bevelDepth);
 	SpriteCodex::DrawBeveledSquareDown(fieldRect, bevelDepth, gfx);
 
 	minefield.Draw(gfx);
 
+	// code for top frame drawing
 	RectI frameTop = gameRect;
 	frameTop.bottom = fieldRect.top;
 	frameTop = frameTop.GetExpanded(bevelDepth - smallBorderSize);
 	SpriteCodex::DrawBeveledSquareDown(frameTop, bevelDepth, gfx);
 
+	// code for face drawing
+	const Vei2 frameTopCenter = frameTop.GetCenter();
+	const int faceSize = SpriteCodex::faceSize;
+	const RectI faceRect = RectI::FromCenter(frameTopCenter, faceSize / 2, faceSize / 2);
+	SpriteCodex::DrawBeveledSquareUp(faceRect, bevelDepth, gfx);
+	const Vei2 facePos(faceRect.left, faceRect.top);
+	SpriteCodex::DrawFaceUnpressed(facePos, gfx);
 }
